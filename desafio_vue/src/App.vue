2194f6ef2dad7f7fa8d6d2433054cb1ce87b5e71
c.munoz58 <c.munoz58@ufromail.cl>
2021-01-18 00:05:53 -0300

    entrega

--- desafio_vue/src/App.vue
+++ desafio_vue/src/App.vue
@@ -1,55 +1,69 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img
-          src="/assents/logo.png"
-          alt=""
-          width="30"
-          height="24"
-          class="d-inline-block align-top"
-        />
-        Bootstrap
-      </a>
+  <div >
+    <div class="navbar navbar-expand-lg navbar-light bg-light" id="nav">
+      <div class="container-fluid" id="container">
+        <a class="navbar-brand" href="#">
+          <img
+            src=""
+            alt=""
+            width="30"
+            height="24"
+            class="d-inline-block align-top"
+          >
+          vuetify
+        </a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-      </ul>
-    </div>
-    </nav>
-    <div class="card">
-      <div class="card-body" v-for="pelicula in peliculas" v-bind:key="pelicula"></div>
-      <button class="btn btn-primary " type="submit" v-on:click="cambiarPagina">Ver Más</button>
+    <div class="row" >
+      <div class="col-4" v-for="pelicula in peliculas" v-bind:key="pelicula" >
+        <v-img  class="img-fluid" src="pelicula.backdrop_path"></v-img>
+        <div class="card-body">
+          <!--Title-->
+          <h4 class="card-title" >{{pelicula.original_title}}</h4>
+          <!--Text-->
+          <p class="card-text">{{pelicula.vote_average}}</p>
+        </div>
+     </div>
     </div>
   </div>
 </template>
 
 <script>
 import ApiService from "./service/api.sevice";
-import Movie from "./models/Movie";
 export default {
   name: "App",
   components: {},
-  data: () => ({
-    pelicula: [],
+  data()  {
+    return{
+      peliculas: [],
+    titulo:null,
+    
     pagina: 1,
-  }),
+    }
+    
+  },
   methods: {
     cambiarPagina: function () {
-      this.pelicula = ApiService.getPopular(this.pagina + 1);
+      this.peliculas = ApiService.getTopRated(this.pagina + 1);
+    
     },
   },
   mounted() {
-    this.pelicula = ApiService.getPopular(this.pagina);
-    this.pelicula = new Movie(this.pelicula);
-  },
+    ApiService.getTopRated(this.pagina).then(response => {
+    this.peliculas =response.data.results});
+    console.log(this.peliculas);
+
+  }
 };
 </script>
 
@@ -61,8 +75,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
-b-navbar-item {
-  text-align: right;
+#nav {
+
+  background-color: #58ACFA ;
+  background: #58ACFA;
 }
+
+#container{
+  color: #58ACFA;
+  background-color: #58ACFA ;
+   background: #58ACFA;
+}
+#navbarNav{
+    background-color: #58ACFA ;
+    background: #58ACFA;
+    
+}
+
 </style>
